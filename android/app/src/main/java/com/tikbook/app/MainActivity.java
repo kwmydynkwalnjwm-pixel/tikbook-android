2f33626c778acd9ed968cc28321b545580a08a3c
Mohammed <kwmydynkwalnjwm@gmail.com>
2026-01-28 18:25:18 +0200

    Save local changes before rebase

---
+++ android/app/src/main/java/com/tikbook/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tikbook.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tikbook.app',
+  appName: 'tikbook',
+  webDir: 'dist'
+};
+
+export default config;
